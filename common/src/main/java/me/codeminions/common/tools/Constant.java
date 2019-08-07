7f5cdc3a944639a0e81ff6a1d2dc5fb929e22bb5
CodeMinions <36119464+CodeMinions@users.noreply.github.com>
2019-08-07 16:05:24 +0800

    Update Constant.java

--- common/src/main/java/me/codeminions/common/tools/Constant.java
+++ common/src/main/java/me/codeminions/common/tools/Constant.java
@@ -1,11 +1,9 @@
 package me.codeminions.common.tools;
 
 public class Constant {
-//    public final static String URL_BASE = "http://192.168.115.79:8080/secread/api";
-//    public final static String URL_BASE = "http://192.168.115.104:8080/secread/api";
-//    public final static String URL_BASE = "http://192.168.115.48:8080/secread/api";
-//    public final static String URL_BASE = "http://192.168.115.70:8080/secread/api";
-    public final static String URL_BASE = "http://192.168.115.59:8080/secread/api";
+
+    // BaseUrl
+    public final static String URL_BASE = "";
 
     public final static String URL_LOGIN = "/account/login";
 
